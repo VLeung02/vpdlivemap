c623f294d750bea76e8a3195dd9c827e8f4a0560
victorl <vleung20@student.cs.ubc.ca>
2022-03-06 21:43:15 -0800

    init ts, angular 4

---
+++ livemap/livemap/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { MapComponent } from './map/map.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ActivityListComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
